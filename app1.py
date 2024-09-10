8bfecb1e5a561657d5febb32dcd772c8b57bb586
Sergey Marchenko <yegres.marchenko@gmail.com>
2024-09-09 18:53:55 -0700

    test

--- app1.py
+++ app1.py
@@ -28,7 +28,6 @@ from datetime           import datetime
     
 st.write("""
 # Update an online DataBase table using data from an AWS file
-# kuku
 # """)
 
 #__________________________________________
